57e9cf480dea150056439d9876ef8a1dd804c1b6
BrunoMateus8817 <bmbsdeveloper5207418@gmail.com>
2025-06-28 09:01:24 -0300

    Adiciona solução do problema 1007 Diferença

---
+++ problema-1007.py
@@ -0,0 +1,14 @@
+# Problema 1007 - Diferença (Beecrowd)
+# Este programa lê quatro inteiros, calcula a diferença entre os produtos e exibe o resultado
+
+# Leitura dos quatro valores inteiros
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+# Cálculo da diferença
+DIFERENCA = (A * B) - (C * D)
+
+# Exibe o resultado
+print(f"DIFERENCA = {DIFERENCA}") 
